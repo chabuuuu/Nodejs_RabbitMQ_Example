b028ae0451b4acfd09ae27579c1af257f67a78c0
chabuuuu <haphuthinh332004@gmail.com>
2023-12-06 11:23:09 +0700

    RabbiMQ pub sub

--- pubsub/post.js
+++ pubsub/post.js
@@ -1,24 +1,30 @@
 const amqplib = require('amqplib');
 const amqp_url_cloud = 'amqps://oxrmbads:huO93XWEqbo-NdbugJUqtt3T_f_RahYs@octopus.rmq3.cloudamqp.com/oxrmbads'
 const amqp_url_docker = 'amqp://localhost:5672'
-const sendQueue = async ({msg}) => {
+
+const postVideo = async ({msg})=> {
     try {
         //1. create connect
         const conn = await amqplib.connect(amqp_url_docker);
         //2. create chanel
         const chanel = await conn.createChannel()
-        //3. create queue name 
-        const nameQueue = 'q1'
-        //4. create queue
-        await chanel.assertQueue(nameQueue, {
+        //3. create exchange
+        const nameExchange = 'video'
+        await chanel.assertExchange(nameExchange, 'fanout', {
             durable: false
         })
-        //5. send to queue
-        await chanel.sendToQueue(nameQueue, Buffer.from(msg))
+        //4. publish video
+        await chanel.publish(nameExchange, '', Buffer.from(msg))
+        console.log('Send OK');
 
+        setTimeout(() => {
+            conn.close();
+            process.exit(0);
+        }, 2000);
     } catch (error) {
         console.log(error.message);
     }
 }
-const msg = process.argv.slice(2).join(' ') || 'Hello'
-sendQueue({msg: msg})
+
+const msg = process.argv.slice(2).join(' ') || 'Hello exchange'
+postVideo({msg: msg})
--- pubsub/receiveNoti.js
+++ pubsub/receiveNoti.js
@@ -1,29 +1,31 @@
 const amqplib = require('amqplib');
 const amqp_url_cloud = 'amqps://oxrmbads:huO93XWEqbo-NdbugJUqtt3T_f_RahYs@octopus.rmq3.cloudamqp.com/oxrmbads'
 const amqp_url_docker = 'amqp://localhost:5672'
-const receiveQueue = async () => {
+
+const receiveNoti = async ()=> {
     try {
         //1. create connect
         const conn = await amqplib.connect(amqp_url_docker);
         //2. create chanel
         const chanel = await conn.createChannel()
-        //3. create queue name 
-        const nameQueue = 'q1'
-        //4. create queue
-        await chanel.assertQueue(nameQueue, {
+        //3. create exchange
+        const nameExchange = 'video'
+        await chanel.assertExchange(nameExchange, 'fanout', {
             durable: false
         })
-        //5. receive to queue
-        await chanel.consume(nameQueue, msg => {
-            console.log('Msg:::', msg.content.toString())
-        }, 
-        {
+        //4. create queue
+        const {queue} = await chanel.assertQueue('')
+        console.log('name of queue:::', queue);
+        //5. Binding
+        await chanel.bindQueue(queue, nameExchange, '')
+        await chanel.consume(queue, msg => {
+            console.log(msg.content.toString());
+        }, {
             noAck: true
-        }
-        )
-
+        })
     } catch (error) {
         console.log(error.message);
     }
 }
-receiveQueue()
+
+receiveNoti()
